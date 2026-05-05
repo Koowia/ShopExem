b2c3f11d7d13e0b74c435887b186d0dc241771a3
Koowia <mironoaartem@gmail.com>
2026-05-05 19:13:59 +0000

    Сделал users и шаблоны к нему

--- ShopExem/settings.py
+++ ShopExem/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 86400 # 1 день
 SESSION_SAVE_EVERY_REQUEST = True
+
+AUTH_USER_MODEL = 'users.CustomUser'
